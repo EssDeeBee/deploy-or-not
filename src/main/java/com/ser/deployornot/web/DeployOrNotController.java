eff9e735f6f1b97ad997e00ca086392884253e0a
Sergey Dzeboev <sergey.dzeboev@drsmile.de>
2021-01-24 04:25:55 +0300

    Post request support added

--- src/main/java/com/ser/deployornot/web/DeployOrNotController.java
+++ src/main/java/com/ser/deployornot/web/DeployOrNotController.java
@@ -4,12 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.text.ParseException;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +15,7 @@ public class DeployOrNotController {
 
 
     @GetMapping("/")
-    public Mono<String> deployOrNot() throws ParserConfigurationException, ParseException {
+    public Mono<String> deployOrNot() {
 
         return WebClient.builder()
                 .baseUrl("https://shouldideploy.today/?tz=Etc%2FGMT%2B1")
@@ -28,6 +26,19 @@ public class DeployOrNotController {
                 .map(Jsoup::parse)
                 .map(document -> document.getElementById("text"))
                 .map(Element::text);
+    }
 
+    @PostMapping("/")
+    public Mono<String> postDeployOrNot() {
+
+        return WebClient.builder()
+                .baseUrl("https://shouldideploy.today/?tz=Etc%2FGMT%2B1")
+                .build()
+                .get()
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(Jsoup::parse)
+                .map(document -> document.getElementById("text"))
+                .map(Element::text);
     }
 }
