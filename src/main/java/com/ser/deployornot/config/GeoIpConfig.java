6cbc83b009a23bda05b23b3e7d345b5b061bcf3e
Sergei <dzeboevsb@gmail.com>
2021-09-25 13:46:39 +0300

    Getting file way has been changed

--- src/main/java/com/ser/deployornot/config/GeoIpConfig.java
+++ src/main/java/com/ser/deployornot/config/GeoIpConfig.java
@@ -3,30 +3,29 @@ package com.ser.deployornot.config;
 import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
-
-import static java.util.Objects.requireNonNull;
 
 @Configuration
 public class GeoIpConfig {
 
     @Bean
     public DatabaseReader asnDatabaseReader() throws IOException {
-        File database = new File(requireNonNull(getClass().getResource("/geoip/GeoLite2-ASN.mmdb")).getFile());
+        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-ASN.mmdb");
         return new DatabaseReader.Builder(database).build();
     }
 
     @Bean
     public DatabaseReader cityDatabaseReader() throws IOException {
-        File database = new File(requireNonNull(getClass().getResource("/geoip/GeoLite2-City.mmdb")).getFile());
+        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-City.mmdb");
         return new DatabaseReader.Builder(database).build();
     }
 
     @Bean
     public DatabaseReader countryDatabaseReader() throws IOException {
-        File database = new File(requireNonNull(getClass().getResource("/geoip/GeoLite2-Country.mmdb")).getFile());
+        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-Country.mmdb");
         return new DatabaseReader.Builder(database).build();
     }
 }
