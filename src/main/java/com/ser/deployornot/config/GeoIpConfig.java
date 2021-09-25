dd1770b59d4709140352c4e2a589407255d0e7e2
Sergei <dzeboevsb@gmail.com>
2021-09-25 14:17:50 +0300

    Open resources changed

--- src/main/java/com/ser/deployornot/config/GeoIpConfig.java
+++ src/main/java/com/ser/deployornot/config/GeoIpConfig.java
@@ -3,29 +3,29 @@ package com.ser.deployornot.config;
 import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class GeoIpConfig {
 
+
     @Bean
     public DatabaseReader asnDatabaseReader() throws IOException {
-        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-ASN.mmdb");
-        return new DatabaseReader.Builder(database).build();
+        InputStream inputStream = getClass().getResource("/geoip/GeoLite2-ASN.mmdb").openStream();
+        return new DatabaseReader.Builder(inputStream).build();
     }
 
     @Bean
     public DatabaseReader cityDatabaseReader() throws IOException {
-        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-City.mmdb");
+        InputStream database = getClass().getResource("/geoip/GeoLite2-City.mmdb").openStream();
         return new DatabaseReader.Builder(database).build();
     }
 
     @Bean
     public DatabaseReader countryDatabaseReader() throws IOException {
-        File database = ResourceUtils.getFile("classpath:geoip/GeoLite2-Country.mmdb");
+        InputStream database = getClass().getResource("/geoip/GeoLite2-Country.mmdb").openStream();
         return new DatabaseReader.Builder(database).build();
     }
 }
--- src/main/java/com/ser/deployornot/web/FinanceNumberController.java
+++ src/main/java/com/ser/deployornot/web/FinanceNumberController.java
@@ -2,15 +2,12 @@ package com.ser.deployornot.web;
 
 import com.ser.deployornot.service.InfoService;
 import lombok.AllArgsConstructor;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-
-import static java.util.Objects.requireNonNull;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -19,14 +16,15 @@ import static org.springframework.http.ResponseEntity.ok;
 public class FinanceNumberController {
 
     private final InfoService infoService;
+    private final ResourceLoader resourceLoader;
 
     @GetMapping(value = "/download/{fileName}", produces = "application/pdf")
     public ResponseEntity<Resource> download(@PathVariable String fileName,
                                              @RequestHeader HttpHeaders httpHeaders) {
         try {
-            File file = new File(requireNonNull(getClass().getResource("/download/" + fileName)).getFile());
+            Resource file = resourceLoader.getResource("classpath:/download/" + fileName);
             infoService.registerDownloading(fileName, httpHeaders);
-            return ok().body(new FileSystemResource(file));
+            return ok().body(file);
         } catch (Exception ex) {
             return ResponseEntity.notFound().build();
         }
